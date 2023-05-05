ec4d5ec940a1a1579484711562b41b00147436bb
Andy Selby <radian@odin.sdf-eu.org>
2023-05-05 11:13:07 +0000

    ~ instead of full path, which will change if I become meta

--- runner.sh
+++ runner.sh
@@ -8,7 +8,7 @@
 # https://stackoverflow.com/questions/42801100/how-to-run-scheduled-scripts-on-linux-without-using-cron
 
 while true; do
-    python main.py /arpa/r/radian/html/reaktor_library_rss.php
+    python main.py ~/html/reaktor_library_rss.php
     # sleep 6 hours
     sleep $((6 * 60 * 60))
 done
